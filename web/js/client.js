ad30fbc1170732299cde6faf9e97f6134cd5ae27
Thaddee Tyl <thaddee.tyl@gmail.com>
2011-11-29 08:57:18 +0100

    Client side done.
    Signed-off-by: Thaddee Tyl <thaddee.tyl@gmail.com>

--- web/js/client.js
+++ web/js/client.js
@@ -2,8 +2,21 @@
  * Copyright © Hexanome H4101 INSA IF.
  */
 
-function stop() {
+function makeexec(action) {
+  Scout('#' + action).on(action, function(query) {
+    query.action = action;
+    query.resp = function(resp) {
+      console.log(action + '-ed');
+    };
+    query.error = function(err) {
+      console.error(err.message);
+    };
+  });
 }
 
-function go() {
-}
+addEventListener('load', function() {
+  makeexec('stop');
+  makeexec('go');
+  makeexec('init');
+}, false);
+
