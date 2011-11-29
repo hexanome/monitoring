ff1a9c75079061095f5890d966f5741597d311d8
Thaddee Tyl <thaddee.tyl@gmail.com>
2011-11-29 09:59:14 +0100

    ScoutCamp update
    Signed-off-by: Thaddee Tyl <thaddee.tyl@gmail.com>

--- web/js/scout.js
+++ web/js/scout.js
@@ -1715,7 +1715,7 @@ Scout = (function Scoutmaker () {
     for (var key in data) {
       if (typeof (jsondata = JSON.stringify(data[key])) === 'string') {
         str += (start? '': '&');
-        str += escape(key) + '=' + escape(jsondata);
+        str += encodeURIComponent(key) + '=' + encodeURIComponent(jsondata);
         if (start) { start = false; }
       }
     }
