125d6ca85983262c0c46975ec4b9135dd87fd1c4
rcatolino <raphael.catolino@gmail.com>
2011-12-06 09:00:59 +0100

    isr done. Need to change msgID
    Signed-off-by: rcatolino <raphael.catolino@gmail.com>

--- VxWorks/CIAI/capteur.c
+++ VxWorks/CIAI/capteur.c
@@ -1,9 +1,12 @@
 #include "capteur.h"
 #include "defs.h"
+#include "mere.h"
+#include <msgQLib.h>
 void isrCapteur()
 {
 	//some new part has been detected
 	int partType=1; //part type is 1
 	part newPart;
 	newPart.type=1;
+	msgQSend(1,&newPart,sizeof(newPart),NO_WAIT,MSG_PRI_NORMAL);
 }
