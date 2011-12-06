1eca558db017057ec02e93b2782dee20186f575c
Jan Keromnes <janx@linux.com>
2011-12-06 11:09:34 +0100

    [Unit test] Server to client info message
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- vxworks.js
+++ vxworks.js
@@ -7,8 +7,10 @@ net.Server(function(socket) {
 
   setInterval(function() {
     var buf = new Buffer(10);
-    socket.write();
+    buf[0] = 'i'; buf[1] = 0; buf[2] = 0; buf[3] = 0; buf[4] = 5;
+    buf[5] = 'F'; buf[6] = 'F'; buf[7] = 'U'; buf[3] = 'U'; buf[4] = 'U';
+    socket.write(buf);
   }, 5000);
 
-}).listen(1337);// 5001 | 3737 
+}).listen(1337);
 
