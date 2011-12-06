14c3cc7bebd0cb359c64c8fab427a902cd6f2d8b
Thaddee Tyl <thaddee.tyl@gmail.com>
2011-12-06 10:56:41 +0100

    Started a test for socket input.
    Signed-off-by: Thaddee Tyl <thaddee.tyl@gmail.com>

--- vxworks.js
+++ vxworks.js
@@ -4,4 +4,11 @@ net.Server(function(socket) {
   socket.on('data',function(data){
     console.log(data+'');
   });
+
+  setInterval(function() {
+    var buf = new Buffer(10);
+    socket.write();
+  }, 5000);
+
 }).listen(1337);// 5001 | 3737 
+
