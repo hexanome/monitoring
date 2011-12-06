b6dc5e83f95ca7fab500a2b13fb58c3835ff4b83
Alexis Jossinet <alexis.jossinet@insa-lyon.fr>
2011-12-06 10:29:09 +0100

    Boulot boulot
    Signed-off-by: Alexis Jossinet <alexis.jossinet@insa-lyon.fr>

--- VxWorks/CIAI/read.h
+++ VxWorks/CIAI/read.h
@@ -1,14 +1,6 @@
-#include "stdio.h"
 #include "sockLib.h"
-
-// faire du tri dans les includes inutiles
-#include "vxWorks.h"
-#include "inetLib.h"
-#include "stdioLib.h"
 #include "strLib.h"
-#include "hostLib.h"
+#include "mere.h"
 
-#define SERVER_PORT_NUM 5001
-#define REPLY_MSG_SIZE 100 // provisoir
-
-int startRead();
+void handlingMessage(char* replyBuf);
+void startRead();
