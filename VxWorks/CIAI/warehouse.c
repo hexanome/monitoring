b1f60762842348fdea5f20a5af17c2c5cf920095
Alexis Jossinet <alexis.jossinet@insa-lyon.fr>
2011-12-09 09:40:29 +0100

    avancement de warehouse.c uniqument sur notepad++
    Signed-off-by: Alexis Jossinet <alexis.jossinet@insa-lyon.fr>

--- VxWorks/CIAI/warehouse.c
+++ VxWorks/CIAI/warehouse.c
@@ -1,34 +1,72 @@
 #include "warehouse.h"
 
+// récupère un nombre dans un message
+int getNumber(int start; int size)
+{
+	for (i = start; i < (start + size - 1); i++)
+			{
+				num += (int) package[i];
+				num *= 256;
+			}
+			num += (int) package[start + size];
+			num_package_type1 += num;
+}
+
 int startWarehouse()
 {
-	int num_package_type1 = 0;
-	int num_package_type2 = 0;
-	char * message_commande = "";	
-	char * package;
-	int i = 0;
-	int num_package = 0;
+	int num_package_type1 = 0; // nombre de palettes de type 1 dans l'entrepot
+	int num_package_type2 = 0; // nombre de palettes de type 2 dans l'entrepot
+	int num_commande = -1; // le nuémro de la commande
+	int num = 0; // variable intérmédiaire utilisée pour récupérer les nombres dans les messages
+	char * package; // message envoyer par la boite aux lettres
+	char * message_commande = ""; // messages envoyé dans la boite aux lettres "messages"	
+	int i = 0; // utilisé dans une boucle for
 	
 	for (;;)
 	{
 		// On attend un message de la boite aux letres "palettes finies" = mid_packaging
-		msgQReceive(mid_packaging ,package, sizeof (&package), WAIT_FOREVER);
+		msgQReceive(mid_packaging, package, sizeof (&package), WAIT_FOREVER);
 		
-		// On récupère le nombre de palette du type donné
-		for (i = 6; i < 9; i++)
+		// On regarde s'il s'agit d'une commande
+		if (package[0] = 'c')
 		{
-			num_package += (int) package[i];
-			num_package *= 256;
+			// On récupère le numéro de la commande
+			num_commande = getNumber(1, 4);
+
+			// On récupère le nombre de palette de type 1
+			num_package_type1 -= getNumber(5, 4);
+		
+			// On récupère le nombre de palette de type 2
+			num_package_type2 -= getNumber(9, 12);
+		
+			// si la commande est prête
+			if ((num_package_type1 > -1)&(num_package_type2 > -1))
+			{
+				// On rajoute des messages dans la boite aux lettres "messages" = mid_log
+				message_commande = "La commande n° " + num_commande + "est prête";
+				msgQSend(mid_log, message_commande, strlen(message_commande), NO_WAIT, MSG_PRI_NORMAL);
+			}
+			// si la commande n'est pas prëte
+			else
+			{
+				// On rajoute des messages dans la boite aux lettres "messages" = mid_log
+				message_commande = "La commande n° " + num_commande + " n'est pas encore prête";
+				msgQSend(mid_log, message_commande, strlen(message_commande), NO_WAIT, MSG_PRI_NORMAL);
+			}
 		}
-		num_package += (int) package[9];
-		
-		// On regarde s'il s'agit d'une commande et si elle est prête
-		if ((num_package_type1 > -1)&(num_package_type2 > -1))
+		// On regarde s'il s'agit d'une initialisation
+		if (package[0] = 'i')
 		{
+			// On récupère le nombre de palette de type 1
+			num_package_type1 += getNumber(1, 4);
+		
+			// On récupère le nombre de palette de type 2
+			num_package_type2 += getNumber(4, 4);
+			
 			// On rajoute un message dans la boite aux lettres "messages" = mid_log
-			message_commande = "Commande prête";
+			message_commande = "Initialisation : Nombre de palettes de type 1 à fabriquer : " + int num_package_type1;
+			message_commande = "Initialisation : Nombre de palettes de type 2 à fabriquer : " + int num_package_type2;
 			msgQSend(mid_log, message_commande, strlen(message_commande), NO_WAIT, MSG_PRI_NORMAL);
 		}
-		
 	}
 }
