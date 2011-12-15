bd2149e5e021e5ebb042dce6a065d22fd679ecdd
rcatolino <raphael.catolino@gmail.com>
2011-12-15 18:45:29 +0100

    Updated warehouse
    Signed-off-by: rcatolino <raphael.catolino@gmail.com>

--- VxWorks/CIAI/warehouse.c
+++ VxWorks/CIAI/warehouse.c
@@ -1,81 +1,70 @@
 #include "defs.h"
 #include "mere.h"
+#include "usine.h"
 #include <strLib.h>
+#include <msgQLib.h>
 #include <stdio.h>
-
-// recupere un nombre dans un message
-static int getNumber(int start, int size)
-{
-	int num = 0; // variable int�rm�diaire utilis�e pour r�cup�rer les nombres dans les messages
-	message package; // message envoye par la boite aux lettres
-	int i = 0; // utilis� dans une boucle for
-	
-	for (i = start; i < (start + size - 1); i++)
-			{
-				num += (int) package[i];
-				num *= 256;
-			}
-			num += (int) package[start + size];
-}
 
 int startWarehouse()
 {
 	int num_package_type1 = 0; // nombre de palettes de type 1 dans l'entrepot
 	int num_package_type2 = 0; // nombre de palettes de type 2 dans l'entrepot
-	int num_commande = -1; // le nu�mro de la commande
-	int num = 0; // variable int�rm�diaire utilis�e pour r�cup�rer les nombres dans les messages
-	message package; // message envoye par la boite aux lettres
-	char * message_commande = ""; // messages envoy� dans la boite aux lettres "messages"	
-	int i = 0; // utilis� dans une boucle for
+	int num_commande = -1; // le numero de la commande
+	int num = 0; // variable intermediaire utilisee pour recuperer les nombres dans les messages
+	pack package; // message envoye par la boite aux lettres
+	message message_commande; // messages envoye dans la boite aux lettres "messages"	
+	int i = 0; // utilise dans une boucle for
 	
 	for (;;)
 	{
 		// On attend un message de la boite aux letres "palettes finies" = mid_packaging
-		msgQReceive(mid_packaging, package, sizeof (&package), WAIT_FOREVER);
+		//Note : cette boite aux lettre contient egalement les palettes qui doivent
+		//partir de l'entrepot. Dans ce cas les membres nbType1 et 2 seront negatifs
+		//pour marquer le depart de l'entrepot de plusieurs palettes, a l'inverse des 
+		//messages de packaging ou il n'arrive que des palettes une par une.
+		msgQReceive(mid_packaging,(char*)&package, sizeof (&package), WAIT_FOREVER);
 		
 		// On regarde s'il s'agit d'une commande
-		if (package[0] == 'c')
+		if (package.nbType1<0 || package.nbType2<0)
 		{
-			// On r�cup�re le num�ro de la commande
-			num_commande = getNumber(1, 4);
+			// On recupere le numero de la commande
+			num_commande = package.number;
 
-			// On r�cup�re le nombre de palette de type 1
-			num_package_type1 -= getNumber(5, 4);
-		
-			// On r�cup�re le nombre de palette de type 2
-			num_package_type2 -= getNumber(9, 12);
-		
-			// si la commande est pr�te
-			if ((num_package_type1 > -1)&(num_package_type2 > -1))
+			// si la commande est pr�te : il y a suffisament de palettes
+			if ((num_package_type1+package.nbType1 > -1)&\
+				(num_package_type2+package.nbType2 > -1))
 			{
 				// On rajoute des messages dans la boite aux lettres "messages" = mid_log
 				sprintf(message_commande,"La commande no %d est prete",num_commande);
+#ifdef test
 				printf("%s\n",message_commande);
-				msgQSend(mid_log, message_commande, strlen(message_commande), NO_WAIT, MSG_PRI_NORMAL);
+#endif
+				msgQSend(mid_log, message_commande, sizeof(message), NO_WAIT,MSG_PRI_NORMAL);
+    			// On recupere le nombre de palette voulues de type 1
+    			num_package_type1 += package.nbType1;
+    			// On r�cup�re le nombre de palette voulues de type 2
+    			num_package_type2 += package.nbType2;
 			}
 			// si la commande n'est pas pr�te
 			else
 			{
 				// On rajoute des messages dans la boite aux lettres "messages" = mid_log
 				
-				sprintf(message_commande,"La commande no %d n'est pas prete",num_commande);
+				sprintf(message_commande," La commande no %d n'est pas prete",num_commande);
+#ifdef test
 				printf("%s\n",message_commande);
-				msgQSend(mid_log, message_commande, strlen(message_commande), NO_WAIT, MSG_PRI_NORMAL);
+#endif
+				msgQSend(mid_log, message_commande, sizeof(message), NO_WAIT, MSG_PRI_NORMAL);
 			}
-		}
-		// On regarde s'il s'agit d'une initialisation
-		if (package[0] == 'i')
-		{
-			// On r�cup�re le nombre de palette de type 1
-			num_package_type1 += getNumber(1, 4);
-		
-			// On r�cup�re le nombre de palette de type 2
-			num_package_type2 += getNumber(4, 4);
+		}else{
+			//Il doit donc s'agir d'une arrivee
+			//On ne recoit les palettes qu'une par une 
+			num_package_type1 += package.nbType1;
+			num_package_type2 += package.nbType2;
 			
 			// On rajoute un message dans la boite aux lettres "messages" = mid_log
-			sprintf(message_commande,"Initialisation : Nombre de palettes de type 1 � fabriquer : %d",num_package_type1);
-			sprintf(message_commande,"Initialisation : Nombre de palettes de type 2 � fabriquer : %d",num_package_type2);
-			msgQSend(mid_log, message_commande, strlen(message_commande), NO_WAIT, MSG_PRI_NORMAL);
+			sprintf(message_commande," Log : nouvelle palette dans l'entrepot de type %d",(package.nbType1==0)? 2 : 1);
+			msgQSend(mid_log, message_commande, sizeof(message), NO_WAIT, MSG_PRI_NORMAL);
 		}
 	}
 }
