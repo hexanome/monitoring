1afc17dcf6a515543e7c05890fc3ec9b5798e485
rcatolino <raphael.catolino@gmail.com>
2011-12-15 23:18:07 +0100

    final commit?
    Signed-off-by: rcatolino <raphael.catolino@gmail.com>

--- VxWorks/CIAI/capteur.c
+++ VxWorks/CIAI/capteur.c
@@ -1,11 +1,12 @@
 #include "defs.h"
 #include "mere.h"
+#include "usine.h"
 #include <msgQLib.h>
 void isrCapteur()
 {
 	//some new part has been detected
-	int partType=1; //part type is 1
 	part newPart;
+	partType=1; //part type is 1
 	newPart.type=1;
 	msgQSend(mid_received_part,(char*)&newPart,sizeof(newPart),NO_WAIT,MSG_PRI_NORMAL);
 }
